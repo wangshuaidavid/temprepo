b1a5d02caec15c64bb80a401cd86ffb31e02ec94
wangshuaidavid <wangshuai@yeah.net>
2014-08-06 21:42:09 +0800

    add error msg

--- src/main/java/cn/enn/rd/HomeEnergyControlLite/controller/UserController.java
+++ src/main/java/cn/enn/rd/HomeEnergyControlLite/controller/UserController.java
@@ -1,15 +1,17 @@
 package cn.enn.rd.HomeEnergyControlLite.controller;
 
+import java.awt.image.RasterFormatException;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
-
-
-
-
+import javax.management.RuntimeErrorException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -51,6 +53,26 @@ public class UserController {
 	public @ResponseBody User tt(@PathVariable Integer id, HttpServletResponse response) {
 
 		List<User> lu = userService.findAll();
+		//throw new RasterFormatException("aaa");
 		return lu.get(0);
+	}
+	
+	@ExceptionHandler
+	public Map<String, Object> exceptionHandler(RuntimeException re) {
+		
+		Map<String, Object> model = new TreeMap<String, Object>();
+		model.put("Status", false);
+		return model;
+		
+	}
+	
+	
+	@RequestMapping(value = "/error")
+	public Map<String, Object> exceptionHandlerGeneral(HttpServletRequest request) {
+		
+		Map<String, Object> model = new TreeMap<String, Object>();
+		model.put("GenStatus", false);
+		return model;
+		
 	}
 }
